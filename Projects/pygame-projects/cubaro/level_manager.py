81de81d0d8fc7b1892c460cd9e3e7b8e7d87e12b
Shivaraj Panishettar <142042222+Shivaraj-Panishettar@users.noreply.github.com>
2025-04-25 20:53:19 +0530

    Delete Projects/pygame-projects/cubaro directory

--- Projects/pygame-projects/cubaro/level_manager.py
+++
@@ -1,7 +0,0 @@
-def load_level(level_number):
-    try:
-        level_module = __import__(f"levels.level{level_number}", fromlist=["create"])
-        return level_module.create()
-    except ImportError:
-        return None  # No more levels
-
